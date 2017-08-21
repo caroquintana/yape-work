0fada9dc732e17679ed175d2b9074a2827bfac17
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-21 17:37:45 -0300

    working on index2 validations

--- assets/js/validations-createUser.js
+++ assets/js/validations-createUser.js
@@ -5,33 +5,37 @@ $(document).ready(function () {
     $("#sign-session").click(onLogin);
 });
 
-function validateForm() {
-    // To Disable Submit Button
-    $("#sign-session").attr('disabled', 'disabled');
-    // Validating Fields
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-    if (!(name == "" || email == "" || password == "")) {
-        if (filter.test(email) & password.length >= 6) {
-            // To Enable Submit Button
-            $("#sign-session").removeAttr('disabled');
-            $("#sign-session").css({
-                "cursor": "pointer",
-                "box-shadow": "1px 0px 6px #333"
-            });
+function validateForm(){
+     // To Disable Submit Button
+        $("#sign-session").attr('disabled', 'disabled');
+        // Validating Fields
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+        if (!(name == "" || email == "" || password == "")) {
+            if (filter.test(email) & password.length >= 6) {
+                // To Enable Submit Button
+                $("#sign-session").removeAttr('disabled');
+                $("#sign-session").css({
+                    "cursor": "pointer",
+                    "box-shadow": "1px 0px 6px #333"
+                });
+            }
         }
-    }
 }
 
-function onLogin() {
+/*function onLogin() {
     if (validateForm()) { //If validate form is True
-        $("#sign-session").css({
+        $('#sign-session').prop('disabled', false);  
+        //$('.sign-session').removeAttr('disabled');       
+    }
+}*/
+function onLogin(){
+    $("#sign-session").css({
             "cursor": "default",
             "box-shadow": "none"
         });
         alert("Datos creados exitosamente..!!");
         window.location.href = 'screen5.html';
-    }
 }
