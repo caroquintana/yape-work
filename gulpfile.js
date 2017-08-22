fdc033e5c395a40be5348ad83ffb891ad0825ff4
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-22 10:37:35 -0300

    index changed to public folder

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('style', function(){
 gulp.task('webserver', function(){
 	gulp.src('../yape-work/')
 	.pipe(webserver({
-		fallback: 'index.html',
+		fallback: 'public/index.html',
 		livereload: true,
 		directoryListing: false,
 		open: true
